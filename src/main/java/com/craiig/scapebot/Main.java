08ac9c509ba1c039df69f7bf2ebdd1b8fd57fbdf
Craig Miller <craigm1357@gmail.com>
2016-03-01 16:03:50 +0000

    Login functionality

--- src/main/java/com/craiig/scapebot/Main.java
+++ src/main/java/com/craiig/scapebot/Main.java
@@ -1,14 +1,41 @@
 package com.craiig.scapebot;
 
+import com.samczsun.skype4j.Skype;
+import com.samczsun.skype4j.SkypeBuilder;
+
+import com.craiig.scapebot.utilities.FileUtilities;
+import com.samczsun.skype4j.Visibility;
+import com.samczsun.skype4j.exceptions.ConnectionException;
+import com.samczsun.skype4j.exceptions.InvalidCredentialsException;
+import com.samczsun.skype4j.exceptions.NotParticipatingException;
+
+import java.util.ArrayList;
+
 /**
- * Created by Craig on 01/03/2016.
+ * Created by Craig on 01/03/2016, 16:03.
+
  */
 
-public class Main {
+class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //main method
+        FileUtilities.directoryExists("data");
+        ArrayList<String> details = FileUtilities.readTextFile("data/login.txt");
+
+        Skype skype = new SkypeBuilder(details.get(0), details.get(1)).withAllResources().build();
+
+        try{
+            skype.login();
+
+            System.out.println("Logged in");
+
+            skype.subscribe();
+            skype.setVisibility(Visibility.ONLINE);
+
+        } catch (ConnectionException | InvalidCredentialsException | NotParticipatingException ex){
+            System.err.println("An error occurred: " + ex.getMessage());
+        }
 
     }
 
---
+++ src/main/java/com/craiig/scapebot/utilities/FileUtilities.java
@@ -0,0 +1,71 @@
+package com.craiig.scapebot.utilities;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Craig on 01/03/2016, 16:02.
+
+ */
+public class FileUtilities {
+
+    public static ArrayList<String> readTextFile(String fileName)
+    {
+        ArrayList<String> result = new ArrayList<>();
+
+        File f = new File(fileName);
+        if (!f.exists())
+        {
+            return result;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
+        {
+            String line = br.readLine();
+
+            while (line != null)
+            {
+                result.add(line);
+                line = br.readLine();
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+
+        }
+
+        return result;
+    }
+
+    public static boolean directoryExists(String directory)
+    {
+        // Check if the directory exists, if not, create it
+        boolean result = false;
+        File dirTest = new File(directory);
+        if (!dirTest.exists())
+        {
+            try
+            {
+                result = dirTest.mkdir();
+
+            } catch (SecurityException e)
+            {
+                e.printStackTrace();
+            }
+
+            // Failed com create directory!
+            if (!result)
+            {
+                System.err.println("failed to create directory");
+            }
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
