ec26ab07ee1423d9927a4f2fae51d5e515583b30
Craig Miller <craigm1357@gmail.com>
2016-03-01 14:04:57 +0000

    Initial commit

---
+++ src/main/java/com/craiig/scapebot/Main.java
@@ -0,0 +1,15 @@
+package com.craiig.scapebot;
+
+/**
+ * Created by Craig on 01/03/2016.
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //main method
+
+    }
+
+}
