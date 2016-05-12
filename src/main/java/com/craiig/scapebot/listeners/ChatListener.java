b208ec26d5c1b6bfe8a8d36fd50f34c196817e53
Craig <craigm1357@gmail.com>
2016-05-12 14:59:21 +0100

    Add timestamps to logged chat messages

--- src/main/java/com/craiig/scapebot/listeners/ChatListener.java
+++ src/main/java/com/craiig/scapebot/listeners/ChatListener.java
@@ -1,6 +1,5 @@
 package com.craiig.scapebot.listeners;
 
-import com.craiig.scapebot.commands.Command;
 import com.samczsun.skype4j.Skype;
 import com.samczsun.skype4j.events.EventHandler;
 import com.samczsun.skype4j.events.Listener;
@@ -8,6 +7,8 @@ import com.samczsun.skype4j.events.chat.message.MessageEvent;
 import com.samczsun.skype4j.exceptions.ConnectionException;
 
 import java.util.Random;
+
+import static com.craiig.scapebot.utilities.CommonUtilities.log;
 
 /**
  * Created by Craig on 01/03/2016, 21:05, 01:16.
@@ -35,9 +36,7 @@ public class ChatListener implements Listener {
             //how to deal with responses being updated dynamically?
             //how to add new words and reload list?
 
-
-
-            System.out.println("Message: '" + e.getMessage().getContent() + "' sent by " + e.getMessage().getSender().getDisplayName() + " in (" + e.getMessage().getSender().getChat().getIdentity() + ")");
+            log(" '" + e.getMessage().getContent() + "' sent by " + e.getMessage().getSender().getDisplayName() + " in (" + e.getMessage().getSender().getChat().getIdentity() + ")");
 
             if(!e.getMessage().getContent().asPlaintext().startsWith("!") && !e.getMessage().getSender().getUsername().equals(skype.getUsername())){
 
