bd2b90adfcacf7a6e10eafb597a61cbb24b49aa3
Craig <craigm1357@gmail.com>
2018-04-21 01:52:57 +0100

    Updated Vis command to use new thread

--- src/main/java/com/craiig/scapebot/commands/VisWax.java
+++ src/main/java/com/craiig/scapebot/commands/VisWax.java
@@ -24,21 +24,29 @@ public class VisWax extends Command {
 
     public void run(ChatMessage msg, List<Command> commands, String trigger) throws ConnectionException {
 
-        String pageURL = "http://services.runescape.com/m=forum/forums.ws?75,76,387,65763383";
+        String pageURL = "http://services.runescape.com/m=forum/forums.ws?75,76,331,66006366";
         Document searchResults = Jsoup.parse(getTextFromUrl(pageURL));
         String message = "";
 
         try{
 
             Elements posts = searchResults.select(".forum-post__body");
-            String prepared = posts.get(1).html()
+            String prepared = posts.get(0).html()
+                    .split("Combination for")[1]
                     .replace("\n", "")
                     .replace("<br>", "\n")
+                    .replace("<b>", "")
                     .replace("\n . \n", "\n")
                     .replace("\n \n", "\n")
                     .replace("\n\n", "\n")
                     .replace("Slot", "\nSlot")
-                    .split("Alternative runes")[0];
+                    .replace("  R ", "R")
+                    .replace("  A ", "A")
+                    .replace("  N ", "N")
+                    .replace("  D ", "D")
+                    .replace("  O ", "O")
+                    .replace("  M ", "M")
+                    .split("Alts are listed")[0];
 
             String[] parts = prepared.split("Slot 1:");
             message = "<b>" + parts[0] + "</b> Slot 1:" + parts[1];
